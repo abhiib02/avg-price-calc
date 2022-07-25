923746e75d87a931952bc8381935473b85be31af
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2022-07-25 21:14:17 +0530

    added avg calc

---
+++ src/App.svelte
@@ -0,0 +1,82 @@
+<script>
+import 'bulma/css/bulma.css';
+
+let inputCounts=1;
+let quantity=[],price=[],cost=[],tquantity=0,tprice=0
+let average=0;
+
+function incinputCount(){
+  inputCounts++;
+}
+function totalquantity(){
+    for (let i = 0; i < quantity.length; i++) {
+      tquantity += parseFloat(quantity[i]);
+    } 
+}
+function totalamount(){
+  for (let i = 0; i < price.length; i++) {
+    tprice += parseFloat(price[i]) * parseFloat(quantity[i]);
+    }
+}
+function avgprice(){
+  average=0;
+  tquantity=0;
+  tprice=0;
+  totalamount()
+  totalquantity()
+  average = tprice / tquantity;
+}
+
+
+</script>
+<main>
+  <section class="hero is-link">
+    <div class="hero-body">
+      <p class="title">
+        Average Price Calculator
+      </p>
+      
+    </div>
+  </section>
+  <hr>
+<div class="container">
+  <div class="columns">
+    <div class="column is-two-thirds">
+      <table class="table is-hoverable is-fullwidth">
+        <thead>
+          <th>S.No</th>
+          <th>Quantity</th>
+          <th>Price</th>
+        </thead>
+        <tbody>
+          {#each Array(inputCounts) as number,index}
+          <tr>
+            <td>{index}</td>
+            <td><input  class="input is-rounded is-small" type="number" bind:value={quantity[index]}></td>
+            <td><input  class="input is-rounded is-small" type="number" bind:value={price[index]}></td>
+          </tr>
+          
+          {/each}
+          <tr>
+            <td><button class="button is-black" on:click={incinputCount} >Add Orders</button></td>
+          </tr>
+
+        </tbody>
+      </table>
+    </div>
+    <div class="column center">
+      <strong>Avg.Price:</strong><h1 class="is-size-1">{average.toFixed(2)}</h1>
+      <button class="button is-black" on:click={avgprice} >Calculate Average price</button>
+    </div>
+    
+  </div>
+ 
+</div>
+</main>
+
+<style>
+ .center{
+  display: grid;
+  place-items: center;
+ }
+</style>
